58364b2042b3156c0d848485fb185da1463bccec
liuy30012-cloud <liuy30012@gmail.com>
2026-04-18 03:17:36 +0800

    config: 添加 Elasticsearch 配置类

---
+++ backend/src/main/java/com/library/config/ElasticsearchConfig.java
@@ -0,0 +1,31 @@
+package com.library.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.elasticsearch.client.ClientConfiguration;
+import org.springframework.data.elasticsearch.client.elc.ElasticsearchConfiguration;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
+
+@Configuration
+@EnableElasticsearchRepositories(basePackages = "com.library.repository")
+public class ElasticsearchConfig extends ElasticsearchConfiguration {
+
+    @Value("${spring.elasticsearch.uris}")
+    private String elasticsearchUris;
+
+    @Value("${spring.elasticsearch.username}")
+    private String username;
+
+    @Value("${spring.elasticsearch.password}")
+    private String password;
+
+    @Override
+    public ClientConfiguration clientConfiguration() {
+        return ClientConfiguration.builder()
+            .connectedTo(elasticsearchUris.replace("http://", ""))
+            .withBasicAuth(username, password)
+            .withConnectTimeout(3000)
+            .withSocketTimeout(5000)
+            .build();
+    }
+}
